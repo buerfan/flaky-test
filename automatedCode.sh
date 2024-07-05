68f3ac790b12056621b2fdb129582da5453d85e4
Md. Erfan <48825550+buerfan@users.noreply.github.com>
2024-07-06 02:51:09 +0600

    Add files via upload
    The code now inputs the issue ID, and test method then is able to store the information in the csv file. The code is now working for CAMEL-12025, and has some unnecessary echo print and hardcoded path.

--- automatedCode.sh
+++ automatedCode.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 
 # Define the issue ID to search for
 #ISSUE_ID="ZOOKEEPER-1045"
 ISSUE_ID=$1
 TEST_METHOD=$2
+
   
 # Fetch all branches and updates
 git fetch --all
@@ -20,6 +38,10 @@ fi
 # Create or clear the report file
 # Create or clear the report file
 REPORT_FILE="commit_report.txt"
+csv_file="test_results.csv"
+
+echo "File Name,Total Tests,Successes,Failures,Errors,Skipped,Total Time (seconds)" > "$csv_file"
+
 > $REPORT_FILE
   
 # Print the commit hashes and messages to the report file
@@ -32,9 +54,49 @@ for COMMIT in $COMMITS; do
   TEST_CLASS_PATH=$(echo "$JAVA_CLASS" | awk -F'src/test/java/' '{print $NF}' | sed 's/\//./g' | sed 's/.java$//')
   echo "Test Class Path $TEST_CLASS_PATH"
   #mvn clean install -DskipTests -pl $MODULE_NAME -am
+  for ((i=1;i<=10;i++)); do
+      echo "Iteration Running $i / 10 : $(date)"
+      mvn -pl $MODULE_NAME test -Dtest=$TEST_CLASS_PATH#$TEST_METHOD | tee mvn-test-$i.log
+      
+      for g in $(egrep "^Running|^\[INFO\] Running " mvn-test-$i.log | rev | cut -d' ' -f1 | rev); do   
+          f=$(find . -name "TEST-${g}.xml" -not -path "*target/surefire-reports/junitreports/*");
+          fcount=$(echo "$f" | wc -l); 
+          echo "Value of - f: $f"
+          
+          for xml_file in $f; do
+              echo "Processing $xml_file"
+    	      testcase_count=$(xmllint --xpath 'count(//testcase)' "$xml_file")
+              echo "Number of test cases: $testcase_count"
 
-  mvn -pl $MODULE_NAME test -Dtest=$TEST_CLASS_PATH#$TEST_METHOD >> $REPORT_FILE 
+	      #testsuite=$(xmllint --xpath '//testsuite' "$xml_file")
+      	      #total_tests=$(xmllint --xpath '//@tests' <<< "$testsuite")
+              #total_failures=$(xmllint --xpath '//@failures' <<< "$testsuite")
+              #total_errors=$(xmllint --xpath '//@errors' <<< "$testsuite")
+              #total_skipped=$(xmllint --xpath '//@skipped' <<< "$testsuite")
+              #total_time=$(xmllint --xpath '//@time' <<< "$testsuite")
 
+              total_tests=$(xmllint --xpath 'string(//testsuite/@tests)' "$xml_file")
+              total_failures=$(xmllint --xpath 'string(//testsuite/@failures)' "$xml_file")
+              total_errors=$(xmllint --xpath 'string(//testsuite/@errors)' "$xml_file")
+              total_skipped=$(xmllint --xpath 'string(//testsuite/@skipped)' "$xml_file")
+              total_success=$(xmllint --xpath 'string(//testsuite/@tests - //testsuite/@failures - //testsuite/@errors - //testsuite/@skipped)' "$xml_file")
+              total_time=$(xmllint --xpath 'string(//testsuite/@time)' "$xml_file") 
+
+
+	      echo "$xml_file,$total_tests,$total_success,$total_failures,$total_errors,$total_skipped,$total_time" >> "$csv_file"
+              #echo "Total Tests: $total_tests"
+              #echo "Total Success: $total_success"
+              #echo "Total Failures: $total_failures"
+              #echo "Total Errors: $total_errors"
+              #echo "Total Skipped: $total_skipped"
+              #echo "Total Time (seconds): $total_time"
+          done
+      done
+
+      mkdir -p /Users/mderfan/camel/AutoScriptResult/isolation/$i
+      mv mvn-test-$i.log /Users/mderfan/camel/AutoScriptResult/isolation/$i
+  done 
+  echo "CSV file created "$csv_file" with test results."
   echo "JAVA CLASS  $JAVA_CLASS"
   echo "MODULE NAME $MODULE_NAME"
   echo "$COMMIT" >> $REPORT_FILE
